2ab4d582ed652ba7a7171342b12834378680ab0a
raj <rpbarmaiya@gmail.com>
2023-10-31 02:58:55 +0530

    SuperAdmin init

--- controllers/superadminControllers.js
+++ controllers/superadminControllers.js
@@ -5,16 +5,26 @@ const Admin = require('../models/superadmin')
 
 module.exports.createAdmin=async(req,res)=>{
     const { name,email,password,number }=req.body
-    const newAdmin = await new Admin({
+    const checkAdmin = await Admin.findOne({email,number})
+    if(checkAdmin){
+        return res.send('Already Exist')
+    }
+    const newAdmin =  new Admin({
         ...req.body
     })
+
+     await newAdmin.save()
     
-    const token = jwt.sign({ userId: user._id }, secretKey);
+    const token = jwt.sign({ userId: newAdmin._id }, process.env.SECRET_KEY);
     
     // // Set the token in a cookie
     res.cookie('token', token, { httpOnly: true });
+    return res.send(newAdmin)
+}
+
+module.exports.renderLogin=async(req,res)=>{
     return res.render('adminlogin',{
-        title:'Login'
+        title:'LOGIN'
     })
 }
 
@@ -24,24 +34,27 @@ module.exports.loginAdmin=async function(req,res){
     
         // Find the user by email
         const admin = await Admin.findOne({ email });
+        console.log(admin);
     
         if (!admin) {
           return res.status(401).json({ error: 'Invalid credentials' });
         }
     
         // Check if the password is correct (handled in the User schema)
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+        const isPasswordValid = await bcrypt.compare(password, admin.password);
         if (!isPasswordValid) {
             return res.status(401).json({ error: 'Invalid credentials' });
         }
     
         // Generate JWT token
-        const token = jwt.sign({ userId: user._id,user }, secretKey);
+        const token = jwt.sign({ userId: admin._id,admin }, process.env.SECRET_KEY);
         
         // Set the token in a cookie
         res.cookie('token', token, { httpOnly: true });
     
-       res.redirect('/');
+       res.render('superadmin',{
+        title:'Dashboard'
+       });
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
--- routes/superadmin.js
+++ routes/superadmin.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const superAdminController = require('../controllers/superadminControllers');
 
+router.post('/create-account',superAdminController.createAdmin)
 
-
+router.get('/login-page',superAdminController.renderLogin)
+router.post('/login',superAdminController.loginAdmin)
 
 
 module.exports = router; 
